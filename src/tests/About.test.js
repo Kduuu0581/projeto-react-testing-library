7c8fd9e9ade013cd0f1fa753eb8283b3a55c3271
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-11-22 15:54:55 -0300

    2. Teste o componente <About.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('2. Teste o componente <About.js />', () => {
+  test('2.1 A página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedex = screen.getByRole('heading', { name: /About Pokédex/i,
+      level: 2 });
+    const img = screen.getByRole('img', { name: /Pokédex/i });
+    expect(pokedex).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
